aed4f74116cd5133b481635233a65eecafc9db54
maitreyee nath <maitreyeenath30798@gmail.com>
2021-08-25 11:36:46 +0530

    style: added css

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 //https://api.funtranslations.com/translate/pirate.json
+//https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json
 
 const inputText=document.querySelector('#inputText');
 const resultText=document.querySelector("#resultText");
 const btn=document.querySelector("#btn-translate");
 
+const URL="https://api.funtranslations.com/translate/pirate.json";
+
+function createURL(text){
+    var textURL=`${URL}?text=${text}`;
+    return textURL;
+}
 
 function display(){
-    console.log(inputText.value);
-    resultText.innerText=inputText.value;
+    fetch(createURL(inputText.value))
+    .then(response=>response.json())
+    .then(json=>{resultText.innerText=json.contents.translated;
+    })
+    
 }
 
 btn.addEventListener("click",display);
